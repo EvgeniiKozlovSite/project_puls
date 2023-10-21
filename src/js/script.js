4065f12bcd2b93ffe25297699aa65787895757d6
Evgen <ekoptik22@gmail.com>
2023-10-21 13:43:06 +0300

    add modal

--- src/js/script.js
+++ src/js/script.js
@@ -58,5 +58,20 @@ toggl('.catalog-item__back');
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back'); */
+// modal
+
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('fast');
+});
+$('.modal__close').on('click', function() {
+$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn('fast');
+  })
+});
 
 });
