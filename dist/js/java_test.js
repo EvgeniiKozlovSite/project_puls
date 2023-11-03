e36b5c87253776b9c56ece65d6e60ea02e2bc3ed
Evgen <ekoptik22@gmail.com>
2023-11-03 16:57:50 +0300

    finish+adaptation

---
+++ dist/js/java_test.js
@@ -0,0 +1,8 @@
+/* let userName;
+userName = prompt(`Как тебя зовут?` , " ");
+alert(`привет, ${userName} !`); */
+
+/* let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b); // 12 */
